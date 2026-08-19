01f18b59a56d8dab64f82692da341706fcaae29a
Hamid Reza Ghavami <hamidr.ghavami@gmail.com>
2026-08-19 09:48:55 +0300

    final changes

--- src/services/SessionManager.js
+++ src/services/SessionManager.js
@@ -1,1 +1,48 @@
 // Manages active session stacks
+import { BoundedStack } from "../ds/BoundedStack.js";
+
+export class SessionManager {
+    myMap = new Map(); // we declare variable in class without DataType in JS
+
+    createSession(sessionId) { 
+        if (!this.myMap.has(sessionId)) { 
+            const session = new BoundedStack();
+            this.myMap.set(sessionId, session);
+        }
+    }
+
+    getSession(sessionId) { 
+        if ( this.myMap.has(sessionId)) { 
+            return this.myMap.get(sessionId);
+        }
+        return null;
+    }
+
+    removeSession(sessionId) {
+        if ( this.myMap.has(sessionId) ) {
+            this.myMap.delete(sessionId);
+        } else {
+            console.log("Error: No session found for this user.");
+        }
+    }
+
+    pushAction(sessionId, actionData) {
+        const userStack = this.getSession(sessionId);
+
+        if ( userStack != null ) { 
+            userStack.funcPush(actionData);
+        } else { 
+            console.log("Error: No session found for this user.");
+        }
+    }
+
+    undoAction(sessionId) { 
+        const userStack = this.getSession(sessionId);
+
+        if ( userStack != null ) { 
+            return userStack.funcPop();
+        }
+    }
+};
+
+export const sessionManagerInstance = new SessionManager();
